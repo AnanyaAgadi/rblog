dbe78c7fe1ee3b23daeeccdfe32276fba9adc936
Ananya <avagadi15@gmail.com>
2021-07-09 16:09:22 +0530

    navbar style and articles add

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.create(title:"Article1", body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+	Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+ image_url:"https://unsplash.com/photos/DC98wz4vNws" , author:"Aby Aby",release:2021-07-11)
+
+Article.create(title:"Article2", body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+	Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+ image_url:"https://unsplash.com/photos/3qG6g8Z2r20" , author:"Aby Aby",release:2021-07-12)
+
+Article.create(title:"Article3", body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+	Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+ image_url:"https://unsplash.com/photos/mGFflvKuekQ" , author:"Aby Aby",release:2021-07-13)
+
+Article.create(title:"Article4", body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+	Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+ image_url:"https://unsplash.com/photos/2NT3cqLN21k" , author:"Aby Aby",release:2021-07-13)
+
+Article.create(title:"Article5", body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+	Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+ image_url:"https://unsplash.com/photos/3MmIZL-8R7M" , author:"Aby Aby",release:2021-07-13)
+
+Article.create(title:"Article6", body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+	Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+ image_url:"https://unsplash.com/photos/N1ogFMg-FaE" , author:"Aby Aby",release:2021-07-13)
+
+Article.create(title:"Article7", body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+	Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+ image_url:"https://unsplash.com/photos/y1Ps7mOAPCw" , author:"Aby Aby",release:2021-07-13)
